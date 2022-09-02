66b0150aeb3f648d04e91d3b91cf7997adf352bc
igornfaustino <igor@nfaustino.com>
2022-09-02 13:15:55 -0300

    fix: return error if request fail

--- src/pages/api/stock/[code].ts
+++ src/pages/api/stock/[code].ts
@@ -1,9 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
+type Data = object;
 
 const key = process.env.HBRASIL_KEY;
 
@@ -14,10 +12,15 @@ export default async function handler(
   if (req.method === "GET") {
     const { code } = req.query;
     const url = `https://api.hgbrasil.com/finance/stock_price?key=${key}&symbol=${code}`;
-    console.log(key, code, url);
-    const stockData = await fetch(url)
-      .then((response) => response.json())
-      .then((data) => data.results[(code as string).toUpperCase()]);
-    res.status(200).json(stockData);
+    try {
+      const stockData = await fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.results[(code as string).toUpperCase()]);
+      res.status(200).json(stockData);
+    } catch (error: any) {
+      res.status(400).json({
+        error: "bad request",
+      });
+    }
   }
 }
